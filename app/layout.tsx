d3b3289a0e44fd496b569a40fd028ced3283711b
Abdullah <abdullahjaved5558@gmail.com>
2026-02-18 18:53:42 +0500

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -54,7 +55,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"
+    suppressHydrationWarning={true}
+  suppressContentEditableWarning={true}
+  >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
