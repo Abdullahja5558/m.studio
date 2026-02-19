13803f368e9cf8e61d453f5fc535edfcf7e78f5e
Abdullah <abdullahjaved5558@gmail.com>
2026-02-19 15:25:30 +0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -31,7 +31,7 @@ const AboutSection = () => {
 
       <div className="max-w-6xl w-full">
         
-        {/* Main Hero About */}
+       
         <div className="text-center space-y-8 mb-32 pt-20">
           <motion.div 
             initial={{ opacity: 0, y: 10 }}
